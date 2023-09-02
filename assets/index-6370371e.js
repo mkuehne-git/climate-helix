95c2672bfd3bc7ee0e0d6e2a6784c1e8f1fdf8a6
mkuehne-git <mk314git@gmail.com>
2023-09-03 00:54:22 +0200

    deploy

--- assets/index-6370371e.js
+++ assets/index-6370371e.js
@@ -4029,7 +4029,7 @@ Year,Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec,J-D,D-N,DJF,MAM,JJA,SON
 <svg xmlns="http://www.w3.org/2000/svg" id="info-icon" height="48" viewBox="0 0 48 48" width="48" >
     <path d="M0 0h48v48h-48z" fill="none"/>
     <path d="M22 34h4v-12h-4v12zm2-30c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 36c-8.82 0-16-7.18-16-16s7.18-16 16-16 16 7.18 16 16-7.18 16-16 16zm-2-22h4v-4h-4v4z"/>
-</svg>`,RE=`<p>The 3D helix visualizes <u>differences to a temperature mean</u> calculated for a specific month with the region of
+</svg>`,RE=`<p>The 3D helix visualizes <u>differences to a temperature mean</u> calculated for a specific month within the region of
     interest <a target="_blank" rel="noopener noreferrer" href="https://data.giss.nasa.gov/gistemp/">[2]</a>.
     Temperature data are shown from 1880 until the beginning of 2023.</p>
 <p>Each loop within the helix is divided into 12 segments, one for each month of the year.
